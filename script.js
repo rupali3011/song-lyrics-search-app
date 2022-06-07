f17de1d1c469ef71c01da0d94ad543de4e28f23a
rupali3011 <102803215+rupali3011@users.noreply.github.com>
2022-06-07 16:56:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const apiURL='https://api.lyrics.ovh';
 //get input value
 form.addEventListener("submit",e=>{
     e.preventDefault();
-   let searchValue=search.value.trim();
+ let searchValue=search.value.trim();
    
-   if(!searchValue){
+   if(searchValue==""){
         alert("Nothing to Search");
     }
     else{
@@ -26,14 +26,14 @@ function displayData(data){
   result.innerHTML = `
                       <ul class="songs">
                       ${data.data
-                      .map(
-                       song => `<li>
+                      .map(song=>
+                        `<li>
                       <div><strong>${song.artist.name}</strong> - ${song.title}
                        </div>
-                      <span data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</span>
+                       <span data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</span>
                       </li>`
         )
-        .join('')}
+        }
     </ul>
   `;
  }
@@ -49,12 +49,17 @@ function displayData(data){
         console.log(songTitle,"oup");
         getLyrics(artist,songTitle);
         }
-    })
+    });
     async function getLyrics(artist,songTitle) {
         const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
         const data = await response.json();
-        const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
-
+        
+        const lyrics = data.lyrics;
+        if (lyrics === undefined){
+            alert('lyrics doesnt exist in this api');
+            console.log('lyrics doesnt exist in this api');
+        }
+        const lyrics1=lyrics.replace(/(\r\n|\r|\n)/g, '</br>');
         result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
-                              <p>${lyrics}</p> `;
+                              <p>${lyrics1}</p> `;
     }
